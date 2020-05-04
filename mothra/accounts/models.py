f96de601936586fa2d80014a2a053376322bef28
Albert Fougy <albert@fougy.com>
2020-05-04 18:21:05 -0400

    test suites for new model

--- mothra/accounts/models.py
+++ mothra/accounts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Grade(models.Model):
+    level = models.IntegerField(
+        validators=[MaxValueValidator(12), MinValueValidator(-1)]
+    )
     name = models.CharField(max_length=2)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering = ("name",)
+        ordering = ("level",)
